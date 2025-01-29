3e532ae7e482aaba640bb241d3e192e2191e44b5
dhanie <romadani.code@gmail.com>
2025-01-29 16:20:13 +0700

    Hand Tracking & DataCollection

---
+++ DataColection.py
@@ -0,0 +1,60 @@
+import cv2
+import os
+from HandTrackingModule import HandDetector
+import numpy as np
+import math
+import time
+
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+
+offset = 20
+imgSize = 300
+
+folder = "train\S"
+if not os.path.exists(folder):
+    os.makedirs(folder)
+
+counter = 0
+
+while True:
+    success, img = cap.read()
+    if not success:
+        print("Gagal membaca frame dari kamera.")
+        break
+
+    hands, img = detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
+
+        # Validasi koordinat cropping
+        y1, y2 = max(0, y - offset), min(img.shape[0], y + h + offset)
+        x1, x2 = max(0, x - offset), min(img.shape[1], x + w + offset)
+        imgCrop = img[y1:y2, x1:x2]
+
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+        aspectRatio = h / w
+
+        if aspectRatio > 1:
+            k = imgSize / h
+            wCal = math.ceil(k * w)
+            imgReSize = cv2.resize(imgCrop, (wCal, imgSize))
+            wGap = math.ceil((imgSize - wCal) / 2)
+            imgWhite[:, wGap:wCal + wGap] = imgReSize
+        else:
+            k = imgSize / w
+            hCal = math.ceil(k * h)
+            imgReSize = cv2.resize(imgCrop, (imgSize, hCal))
+            hGap = math.ceil((imgSize - hCal) / 2)
+            imgWhite[hGap:hCal + hGap, :] = imgReSize
+
+        cv2.imshow("ImageCrop", imgCrop)
+        cv2.imshow("ImageWhite", imgWhite)
+
+    cv2.imshow("Image", img)
+    key = cv2.waitKey(1)
+    if key == ord("s"):
+        counter += 1
+        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg', imgWhite)
+        print(counter)
